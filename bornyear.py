311477a3afc9bc991af003221a2a1efa1e4e615e
MaksimNovozhilov <ufamax@gmail.com>
2022-12-11 20:56:25 +0600

    initial

---
+++ bornyear.py
@@ -0,0 +1,10 @@
+# МОДУЛЬ 2
+
+ASPushkin = 1799
+
+answer = int(input('В каком году родился Пушкин? '))
+
+if answer == ASPushkin:
+    print('Верно')
+else:
+    print('Неверно')
---
+++ variables.py
@@ -0,0 +1,24 @@
+#  МОДУЛЬ !
+
+# название игры
+
+name = 'puzzels'
+
+print(name, type(name))
+
+# ограничение по возрасту
+age = 3
+
+print(age, type(age))
+
+# среднее время сбора,мин
+
+avg_time = 3.5
+
+print(avg_time,type(avg_time))
+
+# Есть ли подсказка
+
+tips = True
+
+print(tips, type(tips))
