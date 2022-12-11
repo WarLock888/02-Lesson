02fe95baed8fecd02f51135958e6dbf03d4b9b30
MaksimNovozhilov <ufamax@gmail.com>
2022-12-11 20:59:04 +0600

    added 3 module

---
+++ bornday.py
@@ -0,0 +1,15 @@
+# МОДУЛЬ 3
+
+ASPushkin = 1799
+
+year = int(input('В каком году родился Пушкин? '))
+
+if year == ASPushkin:
+    date = int(input('Дата рождения Пушкина? '))
+    if date == 6:
+        print('Верно')
+    else:
+        print('Неверно')
+
+else:
+    print('Неверно')
