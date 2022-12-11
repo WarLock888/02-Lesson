8391546d31c25670229645ed9d83ac4a27da9ac2
MaksimNovozhilov <ufamax@gmail.com>
2022-12-11 21:07:07 +0600

    add 5 module

---
+++ borndayforewer.py
@@ -0,0 +1,14 @@
+# МОДУЛЬ 5
+
+ASPushkin = 1799
+
+year = ''
+date = ''
+
+while year != ASPushkin:
+    year = int(input('В каком году родился Пушкин? '))
+
+while date != 6:
+    date = int(input('Дата рождения Пушкина? '))
+
+print('Верно')
